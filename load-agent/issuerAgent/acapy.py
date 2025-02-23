5e1059c598ee36444998ea82b05b27a1f8300b1b
PatStLouis <patrick.st-louis@opsecid.ca>
2025-02-23 21:28:29 +0000

    add parametrized settings for load agent, models for protocol exchanges and locust files subdirectory
    Signed-off-by: PatStLouis <patrick.st-louis@opsecid.ca>

--- load-agent/issuerAgent/acapy.py
+++ load-agent/issuerAgent/acapy.py
@@ -3,37 +3,42 @@ import json
 import os
 import requests
 import time
+from .models import IssueCredentialV1, CredentialProposalV1, AnonCredsRevocation
 
 class AcapyIssuer(BaseIssuer):
+        def __init__(self):
+                self.agent_url = os.getenv("ISSUER_URL")
+                self.headers = json.loads(os.getenv("ISSUER_HEADERS"))
+                self.headers['Content-Type'] = "application/json"
+                self.cred_def_id = os.getenv("CRED_DEF")
+                self.schema_id = os.getenv("SCHEMA")
+                self.cred_attributes = json.loads(os.getenv("CRED_ATTR"))
         
         def get_invite(self, out_of_band=False):
-                headers = json.loads(os.getenv("ISSUER_HEADERS"))
-                headers["Content-Type"] = "application/json"
-
                 if out_of_band:
                         # Out of Band Connection 
                         # (ACA-Py v10.4 - only works with connections protocol, not DIDExchange) 
                         r = requests.post(
-                                os.getenv("ISSUER_URL") + "/out-of-band/create-invitation?auto_accept=true", 
+                                f"{self.agent_url}/out-of-band/create-invitation?auto_accept=true", 
                                 json={
                                 "metadata": {}, 
                                 "handshake_protocols": ["https://didcomm.org/connections/1.0"]
                                 },
-                                headers=headers
+                                headers=self.headers
                         )
                 else:
                         # Regular Connection
                         r = requests.post(
-                                os.getenv("ISSUER_URL") + "/connections/create-invitation?auto_accept=true",
+                                f"{self.agent_url}/connections/create-invitation?auto_accept=true",
                                 json={"metadata": {}, "my_label": "Test"},
-                                headers=headers,
+                                headers=self.headers,
                         )
 
                         # Ensure the request worked
                         try:
-                                try_var = r.json()["invitation_url"]
+                                r.json()["invitation_url"]
                         except Exception:
-                                raise Exception("Failed to get invitation url. Request: ", r.json())
+                                raise Exception("Failed to get invitation url. Request: ", r.text)
                         if r.status_code != 200:
                                 raise Exception(r.content)
 
@@ -43,9 +48,9 @@ class AcapyIssuer(BaseIssuer):
                 if out_of_band:
                         invitation_msg_id = r['invi_msg_id']
                         g = requests.get(
-                                os.getenv("ISSUER_URL") + "/connections",
+                                f"{self.agent_url}/connections",
                                 params={"invitation_msg_id": invitation_msg_id},
-                                headers=headers,
+                                headers=self.headers,
                         )
                         # Returns only one
                         connection_id = g.json()['results'][0]['connection_id']
@@ -58,48 +63,40 @@ class AcapyIssuer(BaseIssuer):
 
         def is_up(self):
                 try:
-                        headers = json.loads(os.getenv("ISSUER_HEADERS"))
-                        headers["Content-Type"] = "application/json"
                         r = requests.get(
-                                os.getenv("ISSUER_URL") + "/status",
+                                f"{self.agent_url}/status",
                                 json={"metadata": {}, "my_label": "Test"},
-                                headers=headers,
+                                headers=self.headers,
                         )
                         if r.status_code != 200:
                                 raise Exception(r.content)
 
                         r = r.json()
-                except:
+                except Exception:
                         return False
 
                 return True
 
         def issue_credential(self, connection_id):
-                headers = json.loads(os.getenv("ISSUER_HEADERS"))
-                headers["Content-Type"] = "application/json"
-
-                issuer_did = os.getenv("CRED_DEF").split(":")[0]
-                schema_parts = os.getenv("SCHEMA").split(":")
-
+                issuer_did = self.cred_def_id.split(":")[0]
+                schema_parts = self.schema_id.split(":")
+                
                 r = requests.post(
-                        os.getenv("ISSUER_URL") + "/issue-credential/send",
-                        json={
-                                "auto_remove": True,
-                                "comment": "Performance Issuance",
-                                "connection_id": connection_id,
-                                "cred_def_id": os.getenv("CRED_DEF"),
-                                "credential_proposal": {
-                                "@type": "issue-credential/1.0/credential-preview",
-                                "attributes": json.loads(os.getenv("CRED_ATTR")),
-                                },
-                                "issuer_did": issuer_did,
-                                "schema_id": os.getenv("SCHEMA"),
-                                "schema_issuer_did": schema_parts[0],
-                                "schema_name": schema_parts[2],
-                                "schema_version": schema_parts[3],
-                                "trace": True,
-                        },
-                        headers=headers,
+                        f"{self.agent_url}/issue-credential/send",
+                        json=IssueCredentialV1(
+                                connection_id=connection_id,
+                                comment='Performance Issuance',
+                                cred_def_id=self.cred_def_id,
+                                issuer_did=issuer_did,
+                                schema_id=self.schema_id,
+                                schema_issuer_did=schema_parts[0],
+                                schema_name=schema_parts[2],
+                                schema_version=schema_parts[3],
+                                credential_proposal=CredentialProposalV1(
+                                        attributes=self.cred_attributes
+                                ).model_dump()
+                        ).model_dump(),
+                        headers=self.headers,
                 )
                 if r.status_code != 200:
                         raise Exception(r.content)
@@ -112,36 +109,24 @@ class AcapyIssuer(BaseIssuer):
                 }
 
         def revoke_credential(self, connection_id, credential_exchange_id):
-                headers = json.loads(os.getenv("ISSUER_HEADERS"))
-                headers["Content-Type"] = "application/json"
-
-                issuer_did = os.getenv("CRED_DEF").split(":")[0]
-                schema_parts = os.getenv("SCHEMA").split(":")
-
                 time.sleep(1)
-
                 r = requests.post(
-                        os.getenv("ISSUER_URL") + "/revocation/revoke",
-                        json={
-                                "comment": "load test",
-                                "connection_id": connection_id,
-                                "cred_ex_id": credential_exchange_id,
-                                "notify": True,
-                                "notify_version": "v1_0",
-                                "publish": True,
-                        },
-                        headers=headers,
+                        f"{self.agent_url}/revocation/revoke",
+                        json=AnonCredsRevocation(
+                                comment='Load Test',
+                                connection_id=connection_id,
+                                cred_ex_id=credential_exchange_id,
+                                notify_version='v1_0'
+                        ).model_dump(),
+                        headers=self.headers,
                 )
                 if r.status_code != 200:
                         raise Exception(r.content)
 
         def send_message(self, connection_id, msg):
-                headers = json.loads(os.getenv("ISSUER_HEADERS"))
-                headers["Content-Type"] = "application/json"
-
                 r = requests.post(
-                        os.getenv("ISSUER_URL") + "/connections/" + connection_id + "/send-message",
+                        f"{self.agent_url}/connections/{connection_id}/send-message",
                         json={"content": msg},
-                        headers=headers,
+                        headers=self.headers,
                 )
-                r = r.json()
+                r.json()
